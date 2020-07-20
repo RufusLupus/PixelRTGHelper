719bb6efec3d46c1bf9e5bed231b76f041bef3a7
RufusLupus <rufus.lupus@outlook.com>
2020-07-20 12:18:16 +0200

    Rename FancyPoint to MarkPoint

--- src/PixelRTGHelper/MarkPoint.java
+++ src/PixelRTGHelper/MarkPoint.java
@@ -3,7 +3,7 @@ package PixelRTGHelper;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 
-public class FancyPoint {
+public class MarkPoint {
     public String name;
     public Point2D position;
     public Color color;
