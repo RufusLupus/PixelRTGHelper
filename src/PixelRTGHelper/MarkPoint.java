d99122c3ac370f8508786ef5fe6746735be88149
RufusLupus <rufus.lupus@outlook.com>
2020-07-20 12:19:11 +0200

    Add getters and setters to fields in MarkPoint

--- src/PixelRTGHelper/MarkPoint.java
+++ src/PixelRTGHelper/MarkPoint.java
@@ -4,7 +4,31 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 
 public class MarkPoint {
-    public String name;
-    public Point2D position;
-    public Color color;
+    private String name;
+    private Point2D position;
+    private Color color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Point2D getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point2D position) {
+        this.position = position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
