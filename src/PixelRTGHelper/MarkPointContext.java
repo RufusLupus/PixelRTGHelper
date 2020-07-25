22fe0bc1a1e732a1861b945e11147e5621ecccdf
RufusLupus <rufus.lupus@outlook.com>
2020-07-25 18:50:25 +0200

    MarkPointContext: add mark points property

--- src/PixelRTGHelper/MarkPointContext.java
+++ src/PixelRTGHelper/MarkPointContext.java
@@ -1,5 +1,7 @@
 package PixelRTGHelper;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -12,6 +14,7 @@ import java.util.List;
 public class MarkPointContext {
     private ArrayList<MarkPoint> markPoints;
     private ObservableList<MarkPoint> observablePoints;
+    private ListProperty<MarkPoint> markPointsProperty;
 
     public ObservableList<MarkPoint> getPoints() {
         return observablePoints;
@@ -20,18 +23,15 @@ public class MarkPointContext {
     public MarkPointContext() {
         markPoints = new ArrayList<MarkPoint>();
         observablePoints = FXCollections.observableList(markPoints);
+        markPointsProperty = new SimpleListProperty<MarkPoint>(observablePoints);
     }
 
-    public void registerListener(ListChangeListener listener){
+    public void registerListener(ListChangeListener listener) {
         observablePoints.addListener(listener);
+        
     }
 
-    /*public void AddPoint(double x, double y)
-    {
-        MarkPoint point = new MarkPoint();
-        point.setPosition(new Point2D(x, y));
-        point.setName("Some point");
-        point.setColor(Color.AQUA);
-        markPoints.add(point);
-    }*/
+    public ListProperty<MarkPoint> getMarkPointsProperty() {
+        return markPointsProperty;
+    }
 }
