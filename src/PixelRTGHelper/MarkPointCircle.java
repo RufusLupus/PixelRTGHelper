198639c0624978295ea30e3fadece05ccba7e099
RufusLupus <rufus.lupus@outlook.com>
2020-07-25 16:23:38 +0200

    Add MarkPointCircle base

---
+++ src/PixelRTGHelper/MarkPointCircle.java
@@ -0,0 +1,12 @@
+package PixelRTGHelper;
+
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+
+public class MarkPointCircle extends Circle {
+
+
+    public MarkPointCircle(MarkPoint markPoint, Node parent) {
+
+    }
+}
