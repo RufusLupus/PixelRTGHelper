622abfcb3e41df7556a059d0a1b389b767dacf01
RufusLupus <rufus.lupus@outlook.com>
2020-07-25 10:00:15 +0200

    ImageViewerController: add listener registering

--- src/PixelRTGHelper/ImageViewerController.java
+++ src/PixelRTGHelper/ImageViewerController.java
@@ -1,10 +1,12 @@
 package PixelRTGHelper;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ImageViewerController implements IMarkPointSharer {
+
     private MarkPointContext context;
 
     @FXML
@@ -16,5 +18,25 @@ public class ImageViewerController implements IMarkPointSharer {
     @Override
     public void setContext(MarkPointContext context) {
         this.context = context;
+        this.context.registerListener(new ListChangeListener() {
+            @Override
+            public void onChanged(Change change) {
+
+            }
+        });
+    }
+    private void updateVisiblePoints() {
+
+    }
+
+    private void addPoint(double x, double y){
+        System.out.println("pos: " + x + ", " + y);
+    }
+
+    @FXML
+    public void initialize() {
+        ImageView.setOnMouseClicked( (e) -> {
+            addPoint(e.getX(), e.getY());
+        });
     }
 }
--- src/PixelRTGHelper/MarkPointContext.java
+++ src/PixelRTGHelper/MarkPointContext.java
@@ -1,7 +1,37 @@
 package PixelRTGHelper;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MarkPointContext {
-    private ArrayList<MarkPoint> markPoints = new ArrayList<MarkPoint>();
+    private ArrayList<MarkPoint> markPoints;
+    private ObservableList<MarkPoint> observablePoints;
+
+    public List<MarkPoint> getPoints() {
+        return observablePoints;
+    }
+
+    public MarkPointContext() {
+        markPoints = new ArrayList<MarkPoint>();
+        observablePoints = FXCollections.observableList(markPoints);
+    }
+
+    public void registerListener(ListChangeListener listener){
+        observablePoints.addListener(listener);
+    }
+
+    /*public void AddPoint(double x, double y)
+    {
+        MarkPoint point = new MarkPoint();
+        point.setPosition(new Point2D(x, y));
+        point.setName("Some point");
+        point.setColor(Color.AQUA);
+        markPoints.add(point);
+    }*/
 }
